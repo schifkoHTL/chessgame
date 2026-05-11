abcbd42b39d030678e0e2a17a50cbfaf2647b831
Student <foobar@sr.htlweiz.at>
2026-05-11 11:03:39 +0200

    horse working halfway

--- ClassLib/Class1.cs
+++ ClassLib/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassLib;
+﻿using System.Net.Http.Headers;
+
+namespace ClassLib;
 
 public class GameField
 {
@@ -8,8 +10,96 @@ public class GameField
     {
         return board[row, col];
     }
+
+    public override string ToString()
+    {
+        string output = "";
+        for(int j = 0; j < 8; j++)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                if(board[j,i] == null)
+                {
+                    output += "   |";
+                }
+                else
+                {
+                    output += $" {board[j,i]!.name} |";
+                }
+
+                if(i == 7 && j != 7)
+                {
+                    output += $"{Environment.NewLine}---+---+---+---+---+---+---+---{Environment.NewLine}";
+                }
+
+
+            }
+        }
+        output += $"{Environment.NewLine}";
+        return output;
+    }
+
+    public bool Move(ChessFigure F, int[] toField)
+    {
+        bool isPossible = false;
+
+        int[] fromField = F.position;
+
+        int rowToField = toField[0];
+        int colToField = toField[1];
+
+        int rowFromField = F.position[0];
+        int colFromField = F.position[1];
+
+        int rowFieldDif;
+        int colFieldDif;
+        if (rowToField < 0 || colToField < 0 || rowToField > 7 || colToField > 7) //falls das feld wohin es soll minus ist oder über 7, scheitert der Zug
+        {
+            return false;
+        }
+
+
+        if (F.name == "H")
+        {
+            
+            rowFieldDif = Math.Abs(rowFromField - rowToField);
+            colFieldDif = Math.Abs(colFromField - colToField);
+
+            if ((rowFieldDif == 1 && colFieldDif == 2) || (rowFieldDif == 2 && colFieldDif == 1))
+            {
+                F.position = [rowToField, colToField];
+                board[rowFromField, colFromField] = null;
+                board[rowToField, colToField] = F;
+
+                isPossible = true;
+                return isPossible;
+            }
+        }
+
+
+        return isPossible;
+    }
 }
 
 public class ChessFigure
 {
+    public string name;
+    public int[] position;
+
+    public ChessFigure(string namen, int[] pos)
+    {
+        name = namen;
+        position = pos;
+    }
+
+    
 }
+
+/*
+King = K
+Queen = Q
+Pawn = P
+Rook = R
+Springer = S
+Bishop = B
+*/
--- Program/Program.cs
+++ Program/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static int Main()
     {      
+        ChessFigure Springer1 = new("H", [0,5]);
+        GameField Field = new();
+
+        Field.board[Springer1.position[0], Springer1.position[1]] = Springer1;
+        Console.WriteLine(Field);
+
+        Field.Move(Springer1, [2,4]);
+        Console.WriteLine(Field);
         return 0;
     }
 }
